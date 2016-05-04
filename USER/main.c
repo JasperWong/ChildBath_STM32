148f4899de6b541b5f01a313df43ba50f1f7478a
JasperWong <766453486@qq.com>
2016-05-04 21:48:17 +0800

    finish int to str

--- USER/main.c
+++ USER/main.c
@@ -4,7 +4,8 @@
 #include "bsp_dht11.h"
 #include "bsp_usart1.h"
 #include "bsp_ds18b20.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 
 DHT11_Data_TypeDef DHT11_Data;
 
@@ -28,22 +29,26 @@ int main(void)
     OLED_Init();
     OLED_Clear();
     
+    u8 room_temp[2];
+    u8 *water_temp;
+    int dec_pl, sign, ndigits = 3; 
 	while(1)
 	{
-        OLED_Clear();
+        printf("\r\ntemperature %.1f\r\n",DS18B20_Get_Temp());
+        water_temp=fcvt(DS18B20_Get_Temp,ndigits,&dec_pl,&sign);
         
-		printf("\r\ntemperature %.1f\r\n",DS18B20_Get_Temp());	
-		
 		Delay_ms(10);
 
 		if( Read_DHT11(&DHT11_Data)==SUCCESS)
 		{
 			printf("\r\nÎÂ¶ÈÎª %d¡æ \r\n",DHT11_Data.temp_int);
+            sprintf(room_temp, " %d" ,DHT11_Data.temp_int);
 		}
+        OLED_Clear();
 		OLED_ShowString(0,0,"room temperature:");
-        OLED_ShowString(18,2,"27");
+        OLED_ShowString(6,2,room_temp);
         OLED_ShowString(0,4,"water temperature:");
-        OLED_ShowString(22,6,"27");
+        OLED_ShowString(20,6,"27");
 		Delay_ms(2000);
 	}
 	  
